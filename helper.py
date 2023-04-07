6872b3cc7930a2458a75ebe5249ebd083995a4c8
Felix Haumann <fhaumann@studenten.hs-bremerhaven.de>
2023-04-07 19:53:47 +0200

    Initial commit

---
+++ helper.py
@@ -0,0 +1,31 @@
+import string
+import pandas as pd
+
+
+def generate_letter_combinations(length:int=3):
+    if length < 1 or length > 3:
+        raise ValueError("Length must be between 1 and 3.")
+    else:
+        for c1 in string.ascii_uppercase:
+            if length == 1:
+                yield c1
+            else:
+                for c2 in string.ascii_uppercase:
+                    if length == 2:
+                        yield c1 + c2
+                    else:
+                        for c3 in string.ascii_uppercase:
+                            yield c1 + c2 + c3
+
+
+def save_df_as_csv(df_to_save: pd.DataFrame, csv_name_no_filetype: str):
+    csv_base_path = r"C:\Users\fhaum\OneDrive\401 MASTER - Masterarbeit\04 Kalkulationen\pythonProject\csv_files"
+    csv_full_path = csv_base_path + "\\" + csv_name_no_filetype + ".csv"
+    df_to_save.to_csv(csv_full_path, index=False, header=False)
+
+
+def read_df_from_csv(csv_name_no_filetype: str):
+    csv_base_path = r"C:\Users\fhaum\OneDrive\401 MASTER - Masterarbeit\04 Kalkulationen\pythonProject\csv_files"
+    csv_full_path = csv_base_path + "\\" + csv_name_no_filetype + ".csv"
+    return_df = pd.read_csv(csv_full_path, header=None, index_col=None)
+    return return_df
